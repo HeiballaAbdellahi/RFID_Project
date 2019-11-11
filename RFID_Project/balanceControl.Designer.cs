9c5ce6efc6514b18caa185475cbc0898640e0f74
Heiballah Abdellahi <abdellahmedlemine@gmail.com>
2019-11-11 13:25:00 +0100

    itss me

--- RFID_Project/balanceControl.Designer.cs
+++ RFID_Project/balanceControl.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RFID_Project
 {
-    partial class UserControl1
+    partial class balanceControl
     {
         /// <summary> 
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(balanceControl));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
--- RFID_Project/balanceControl.cs
+++ RFID_Project/balanceControl.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RFID_Project
 {
-    public partial class UserControl1 : UserControl
+    public partial class balanceControl : UserControl
     {
-        public UserControl1()
+        public balanceControl()
         {
             InitializeComponent();
         }
